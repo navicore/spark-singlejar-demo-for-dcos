32cc4124c1eb36293a2b2da9b4dfd783d2a55dc0
Ed Sweeney <ed@table.co>
2016-07-07 20:19:56 -0700

    np

--- build.sbt
+++ build.sbt
@@ -1,9 +1,13 @@
-name         := "Spark Single Jar Demo"
+name         := "sparkdemo"
 version      := "1.0"
 organization := "OnExtent"
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.10.4"//
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.0" % "provided"
 
 resolvers += Resolver.mavenLocal
+
+mainClass in assembly := Some("onextent.spark.demo.countwords.CountFromUrl")
+assemblyJarName in assembly := "countwords.jar"
+
--- src/main/scala/onextent/spark/demo/countwords/CountFromUrl.scala
+++ src/main/scala/onextent/spark/demo/countwords/CountFromUrl.scala
@@ -1,12 +1,26 @@
 package onextent.spark.demo.countwords
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * Created by navicore on 7/7/16.
   */
-class CountFromUrl {
+object CountFromUrl {
 
-  val html = scala.io.Source.fromURL("https://spark.apache.org/").mkString
-  val list = html.split("\n").filter(_ != "")
-  val rdds = sc.parallelize(list)
-  val count = rdds.filter(_.contains("Spark")).count()
+  def main(args: Array[String]) {
+    System.setProperty("spark.mesos.coarse", "true")
+    val MESOS_MASTER = System.getProperty("MESOS_MASTER","dcos-master-42450089-0")
+    val conf = new SparkConf()
+      .setMaster(s"mesos://$MESOS_MASTER:5050")
+      //.setMaster("local")
+      .setAppName("Count From Url")
+    //  .set("spark.executor.uri", "<path to spark-1.6.2.tar.gz uploaded above>")
+    val sc = new SparkContext(conf)
+    val html = scala.io.Source.fromURL("https://spark.apache.org/").mkString
+    val list = html.split("\n").filter(_ != "")
+    val rdds = sc.parallelize(list)
+    val count = rdds.filter(_.contains("Spark")).count()
+    println(s"see this: $count")
+  }
 }
+
